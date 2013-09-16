cac8733eda91e6991dc4a4ed7ac30ed223fc8f41
Knud Möller <dunken69@gmail.com>
2013-09-16 18:09:09 +0200

    IAuthFunctions: site_read
    no one is allowed to read anything

--- ckanext/diachron/plugin.py
+++ ckanext/diachron/plugin.py
@@ -5,9 +5,14 @@ import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+
+def site_read(context, data_dict=None):
+    return {'success': False, 'msg': 'No one is allowed to see anything'}
+
 class DiachronPlugin(plugins.SingletonPlugin):
     
     plugins.implements(plugins.IConfigurer, inherit=False)
+    plugins.implements(plugins.IAuthFunctions)
     
     def update_config(self, config):  
         our_public_dir = os.path.join('theme', 'public')
@@ -21,4 +26,5 @@ class DiachronPlugin(plugins.SingletonPlugin):
         config['ckan.site_title'] = "Diachron Data Repository"
         config['ckan.site_logo'] = "/base/images/diachron_logo.png"
 
-        
+    def get_auth_functions(self):
+        return {'site_read': site_read}
