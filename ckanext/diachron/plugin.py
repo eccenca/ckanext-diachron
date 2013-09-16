4f1787e63314fa02dd9fc9ea95bee36b12ee22b4
Knud Möller <dunken69@gmail.com>
2013-09-16 16:29:22 +0200

    implementing iConfigurer, added logo

--- ckanext/diachron/plugin.py
+++ ckanext/diachron/plugin.py
@@ -2,4 +2,16 @@ import ckan.plugins as plugins
 
 
 class DiachronPlugin(plugins.SingletonPlugin):
-    pass
+    
+    plugins.implements(plugins.IConfigurer, inherit=False)
+    
+    def update_config(self, config):  
+        our_public_dir = os.path.join('theme', 'public')
+        template_dir = os.path.join('theme', 'templates')
+
+        # overriding configuration fields:
+        # set our local template and resource overrides
+        toolkit.add_public_directory(config, our_public_dir)
+        toolkit.add_template_directory(config, template_dir)
+
+    
