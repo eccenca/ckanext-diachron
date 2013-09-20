5b3f6b21e8d425e7bdf4100be0f2c9367c387c3e
Knud Möller <dunken69@gmail.com>
2013-09-20 23:45:21 +0200

    preventing dataset_read to anonymous users

--- ckanext/diachron/plugin.py
+++ ckanext/diachron/plugin.py
@@ -4,9 +4,25 @@ import os
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import ckan.logic.validators as logic_validators
 
 log = logging.getLogger(__name__)
+
+def dataset_read(context, data_dict=None):
+    # Get the user name of the logged-in user.
+    user_name = context['user']
+    
+    # We have the logged-in user's user name, get their user id.
+    convert_user_name_or_id_to_id = toolkit.get_converter(
+        'convert_user_name_or_id_to_id')
+    try:
+        user_id = convert_user_name_or_id_to_id(user_name, context)
+    except toolkit.Invalid:
+        # The user doesn't exist (e.g. they're not logged-in).
+        return {'success': False,
+                'msg': 'You must be logged-in as a member of the curators '
+                       'group to create new groups.'}
+    
+    return {'success': True, 'msg': 'You are logged in, good.'}
 
 def site_read(context, data_dict=None):
     # Get the user name of the logged-in user.
@@ -17,7 +33,7 @@ def site_read(context, data_dict=None):
         'convert_user_name_or_id_to_id')
     try:
         user_id = convert_user_name_or_id_to_id(user_name, context)
-    except logic_validators.Invalid:
+    except toolkit.Invalid:
         # The user doesn't exist (e.g. they're not logged-in).
         return {'success': False,
                 'msg': 'You must be logged-in as a member of the curators '
@@ -43,4 +59,7 @@ class DiachronPlugin(plugins.SingletonPlugin):
         config['ckan.site_logo'] = "/base/images/diachron_logo.png"
 
     def get_auth_functions(self):
-        return {'site_read': site_read}
+        return {
+            'site_read': site_read,
+            'dataset_read': dataset_read
+        }
